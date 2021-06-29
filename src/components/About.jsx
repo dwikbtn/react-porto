6917542ff7c3101a54a3c31562541c7153e34e35
Dwi Darma <dwikbtn15@gmail.com>
2021-06-29 10:37:28 +0800

    add skill list on about page not yet style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import Brand from "./Brand";
 
 Modal.setAppElement("#root");
 
@@ -10,6 +9,24 @@ const About = () => {
   function toggleModal() {
     setIsOpen(!isOpen);
   }
+
+  const frontEndSkill = [
+    {skillName: 'javascript'},
+    {skillName: 'react'},
+    {skillName: 'gatsby'},
+    {skillName: 'next.js'},
+    {skillName: 'css.'},
+    {skillName: 'bootstrap'},
+    {skillName: 'tailwind'},
+    {skillName: 'SASS'},
+  ]
+  const backEndSkill = [
+    {skillName: 'nodejs'},
+    {skillName: 'express'},
+    {skillName: 'psql'},
+    {skillName: 'mongodb'},
+    {skillName: 'firebase'},
+  ]
 
   return (
     <>
@@ -93,134 +110,38 @@ const About = () => {
               <div className="my_box">
                 <div className="left">
                   <div className="about_title">
-                    <h3>Photography Skills</h3>
+                    <h3>Front End</h3>
                   </div>
                   {/* END ABOUT TITLE */}
-
-                  <div className="tokyo_progress">
-                    <div className="progress_inner" data-value="95">
-                      <span>
-                        <span className="label">Wedding Photography</span>
-                        <span className="number">95%</span>
-                      </span>
-                      <div className="background">
-                        <div className="bar">
-                          <div className="bar_in" style={{width: 95 + '%'}}></div>
-                        </div>
+                  <div className="skill-list">
+                    {frontEndSkill.map((skill, index) =>(
+                      <div  key={index}>
+                        <img src={`/assets/img/skill/frontend/image${index+1}.png`} alt={`stack ${skill.skillName}`} />
+                       <p>{skill.skillName}</p>
                       </div>
-                    </div>
-
-                    <div className="progress_inner" data-value="80">
-                      <span>
-                        <span className="label">Lifestyle Photography</span>
-                        <span className="number">80%</span>
-                      </span>
-                      <div className="background">
-                        <div className="bar">
-                        <div className="bar_in" style={{width: 80 + '%'}}></div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="progress_inner" data-value="90">
-                      <span>
-                        <span className="label">Family Photography</span>
-                        <span className="number">90%</span>
-                      </span>
-                      <div className="background">
-                        <div className="bar">
-                        <div className="bar_in" style={{width: 90 + '%'}}></div>
-                        </div>
-                      </div>
-                    </div>
+                     ))}
                   </div>
-                  {/* END PROGRESS */}
                 </div>
                 {/* END LEFT */}
 
                 <div className="right">
                   <div className="about_title">
-                    <h3>Language Skills</h3>
+                    <h3>Back End</h3>
+                  </div>
+                  <div className="skill-list">
+                    {backEndSkill.map((skill, index) =>(
+                      <div  key={index}>
+                        <img src={`/assets/img/skill/backend/image${index+1}.png`} alt={`stack ${skill.skillName}`} />
+                       <p>{skill.skillName}</p>
+                      </div>
+                     ))}
                   </div>
                   {/* END TITLE */}
-                  <div className="tokyo_progress">
-                    <div className="progress_inner" data-value="95">
-                      <span>
-                        <span className="label">English</span>
-                        <span className="number">95%</span>
-                      </span>
-                      <div className="background">
-                        <div className="bar">
-                        <div className="bar_in" style={{width: 95 + '%'}}></div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="progress_inner" data-value="90">
-                      <span>
-                        <span className="label">Japanese</span>
-                        <span className="number">90%</span>
-                      </span>
-                      <div className="background">
-                        <div className="bar">
-                        <div className="bar_in" style={{width: 90 + '%'}}></div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="progress_inner" data-value="85">
-                      <span>
-                        <span className="label">Arabian</span>
-                        <span className="number">85%</span>
-                      </span>
-                      <div className="background">
-                        <div className="bar">
-                        <div className="bar_in" style={{width: 85 + '%'}}></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {/* EDN TOKYO PROGRESS */}
                 </div>
                 {/* END RIGHT */}
               </div>
               {/* END MYBOX */}
-
-              <div className="counter">
-                <div className="about_title">
-                  <h3>Fun Facts</h3>
-                </div>
-                <ul>
-                  <li>
-                    <div className="list_inner">
-                      <h3>777+</h3>
-                      <span>Projects Completed</span>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="list_inner">
-                      <h3>3K</h3>
-                      <span>Happy Clients</span>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="list_inner">
-                      <h3>9K+</h3>
-                      <span>Lines of Code</span>
-                    </div>
-                  </li>
-                </ul>
-                {/* END COUNTER CONTENT */}
-              </div>
-              {/* END COUNTER */}
-
-              <div className="partners">
-                <div className="about_title">
-                  <h3>Our Partners</h3>
-                </div>
-                <Brand />
-              </div>
-              {/* END PARTNER SLIDER */}
+              
             </div>
           </div>
         </div>
