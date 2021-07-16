406139720544dbc9dc80e117f0a911c29d3a1c50
unknown <dwikbtn15@gmail.com>
2021-07-16 17:10:19 +0800

    adding dummy data

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 
-const Cards = () => {
+const Cards = (props) => {
+  // console.log(props.data[0].id);
     return (
       <div className="card-container">
-        <div className="porto-img">
-          <img src="assets/img/slider/1.jpg" alt="porto" />
-        </div>
-        <h1>Lorem.</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Consequuntur, laudantium praesentium. Atque vel labore magni,
-          similique porro quisquam nulla quae!
-        </p>
-        <button className="porto-btn github">Github</button>
-        <button className="porto-btn demo">Live Demo</button>
+        {props.children}
       </div>
     );
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,54 +2,65 @@ import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import Cards from "./Cards";
 
-const Portfolio = () => {
+//json
+import data from "../porto.json";
+
+const Portfolio = (props) => {
   return (
     <>
-        <div className="tokyo_tm_portfolio">
-          <div className="tokyo_tm_title">
-            <div className="title_flex">
-              <div className="left">
-                <span>Portfolio</span>
-                <h3>All My Works</h3>
-              </div>
+      <div className="tokyo_tm_portfolio">
+        <div className="tokyo_tm_title">
+          <div className="title_flex">
+            <div className="left">
+              <span>Portfolio</span>
+              <h3>All My Works</h3>
             </div>
           </div>
-          {/* END TOKYO_TM_TITLE */}
-          <div className="portfolio_filter">
-            <Tabs>
-              <TabList>
-                <Tab>All</Tab>
-                <Tab>Front-End</Tab>
-                <Tab>Back-End</Tab>
-                <Tab>Others</Tab>
-              </TabList>
-              {/* END TABLIST */}
-              <div className="list_wrapper">
-                  <TabPanel>
-                    <Cards/>
-                  </TabPanel>
-                  {/* END ALL PORTFOLIO GALLERY */}
-
-                  <TabPanel>
-                   
-                  </TabPanel>
-                  {/* END Front end list */}
-
-                  <TabPanel>
-                    
-                  </TabPanel>
-                  {/* END backend list */}
-
-                  <TabPanel>
-                    
-                  </TabPanel>
-                  {/* END others list GALLERY */}
-                {/* END TABPANEL */}
-              </div>
-              {/* END LIST WRAPPER */}
-            </Tabs>
-          </div>
         </div>
+        {/* END TOKYO_TM_TITLE */}
+        <div className="portfolio_filter">
+          <Tabs>
+            <TabList>
+              <Tab>All</Tab>
+              <Tab>Front-End</Tab>
+              <Tab>Back-End</Tab>
+              <Tab>Others</Tab>
+            </TabList>
+            {/* END TABLIST */}
+            <div className="list_wrapper">
+              <TabPanel>
+                {/* <Cards data={data.portofolios}/> */}
+                {data.portofolios.map(porto => {
+                  return (
+                  <Cards key={porto.id}>
+                    <div className="porto-img">
+                      <img src={porto.imageUrl} alt={porto.header} />
+                    </div>
+                    <h1>{porto.header}</h1>
+                    <p>
+                      {porto.description}
+                    </p>
+                    <button className="porto-btn github">Github</button>
+                    <button className="porto-btn demo">Live Demo</button>
+                  </Cards>)
+                })}
+              </TabPanel>
+              {/* END ALL PORTFOLIO GALLERY */}
+
+              <TabPanel></TabPanel>
+              {/* END Front end list */}
+
+              <TabPanel></TabPanel>
+              {/* END backend list */}
+
+              <TabPanel></TabPanel>
+              {/* END others list GALLERY */}
+              {/* END TABPANEL */}
+            </div>
+            {/* END LIST WRAPPER */}
+          </Tabs>
+        </div>
+      </div>
     </>
   );
 };
